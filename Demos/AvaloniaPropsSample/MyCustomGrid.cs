fd384f28dc892b6161e5553c73615f566af133df
Nick Polyak <nick.polyak@morganstanley.com>
2022-07-11 13:52:17 -0400

    created Avalonia Properties sample

---
+++ Demos/AvaloniaPropsSample/MyCustomGrid.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace AvaloniaPropsSample
+{
+    public class MyCustomGrid : Grid
+    {
+
+        #region TheText Styled Avalonia Property
+        public string TheText
+        {
+            get { return GetValue(TheTextProperty); }
+            set { SetValue(TheTextProperty, value); }
+        }
+
+        public static readonly StyledProperty<string> TheTextProperty =
+            AvaloniaProperty.Register<MyCustomGrid, string>
+            (
+                nameof(TheText)
+            );
+        #endregion TheText Styled Avalonia Property
+
+    }
+}
---
+++ Demos/AvaloniaPropsSample/MyCustomTextBlock.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace AvaloniaPropsSample
+{
+    public class MyCustomTextBlock : TextBlock
+    {
+
+        #region TheText Direct Avalonia Property
+        private string? _TheText = null;
+
+        public static readonly DirectProperty<MyCustomTextBlock, string?> TheTextProperty =
+            AvaloniaProperty.RegisterDirect<MyCustomTextBlock, string?>
+            (
+                nameof(TheText),
+                o => o.TheText,
+                (o, v) => o.TheText = v
+            );
+
+        public string? TheText
+        {
+            get => _TheText;
+            set
+            {
+                SetAndRaise(TheTextProperty, ref _TheText, value);
+            }
+        }
+
+        #endregion TheText Direct Avalonia Property
+
+    }
+}
