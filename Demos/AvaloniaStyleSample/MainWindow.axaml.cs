9283359649d869ed2a984155bf3885a7bb4578ca
Nick Polyak <nick.polyak@morganstanley.com>
2022-07-11 16:36:58 -0400

    styles sample

---
+++ Demos/AvaloniaStyleSample/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using System;
+
+namespace AvaloniaStyleSample
+{
+    public partial class MainWindow : Window
+    {
+        IDisposable _subscription;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Demos/AvaloniaStyleSample/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace AvaloniaStyleSample
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
